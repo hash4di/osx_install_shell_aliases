e9fa049d16055872e990bee0437913169ca6c9cb
hash4di <hash4di@VoidRay.local>
2020-09-20 16:36:33 +0200

    fix path

--- shell_aliases.d/install_osx_crond.sh
+++ shell_aliases.d/install_osx_crond.sh
@@ -1,5 +1,5 @@
 function install_osx_crond() {
-  test -d "$HOME/projects/osx_crond" || git clone git@github.com:pr0d1r2/osx_crond.git "$HOME/projects/osx_crond" || return $?
+  test -d "$HOME/projects/osx_crond" || git clone git@github.com:hash4di/osx_crond.git "$HOME/projects/osx_crond" || return $?
   cd "$HOME/projects/osx_crond" || return $?
   sh setup.sh || return $?
 }
